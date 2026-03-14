a1def28039c7b6d32cf5dbc758de0f307aa1021e
John Reniel Cruz <johnreniel042206@gmail.com>
2026-03-14 20:59:06 +0800

    style: add class_names and fix signal declaration order

--- Scripts/Shelf.gd
+++ Scripts/Shelf.gd
@@ -1,3 +1,4 @@
+class_name Shelf
 extends StaticBody3D
 
 signal pressed
@@ -5,9 +6,8 @@ signal pressed
 @onready var area_3d: Area3D = $Area3D
 
 func _ready() -> void:
-    # Connect the input_event signal from Area3D
-    area_3d.input_event.connect(_on_input_event)
+	area_3d.input_event.connect(_on_input_event)
 
 func _on_input_event(_camera: Node, event: InputEvent, _position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
-    if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-        pressed.emit()
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		pressed.emit()
